f0378dd11ef74d8cdad6297bf873981355a92295
fantauzzi <Francesco_Fantauzzi@yahoo.com>
2022-09-14 14:00:32 +0200

    Implemented metrics reporting on the test set and saving of the best model after training

--- src/census_preprocess.py
+++ src/census_preprocess.py
@@ -1,23 +1,49 @@
+import logging
 import pandas as pd
+from pathlib import Path
 
+logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
+logger = logging.getLogger()
 
-def main():
-    raw_datafile = '../data/census.csv'
-    cleaned_datafile = '../data/census_cleaned.csv'
-    # categorical_idx = (1, 3, 4, 5, 6, 7, 8, 9, 13)
+def str_columns(df: pd.DataFrame) -> list[str]:
+    """
+    Returns the list of column names from a given Pandas dataframe where the columns are of type object (i.e.
+    they contain strings).
+    :param df: The given dataframe.
+    :type df: pandas.DataFrame
+    :return: The requested list of column names.
+    :rtype: list[str]
+    """
+    str_column_names = [name for name in df.columns if pd.api.types.is_string_dtype(df[name])]
+    return str_column_names
 
+def pre_process(raw_datafile: str, cleaned_datafile: str):
+    """
+    Clean-up the census dataset, and save the result in a file.
+    Clean-up does the following:
+     - remove any trailing space from the column names (in the csv file header)
+     - for every column that doesn't contain numeric data, remove any trailing space from its values
+     - the `salary` column is turned to numerical, replacing its values `>50K` and `<=50K` with 1 and 0 respectively
+     - every `?` value (in non-numerical columns) is replaced with `null`
+    :param raw_datafile: the file containing the census dataset to be cleaned-up
+    :type raw_datafile: string
+    :param cleaned_datafile: the file that will contain the result; if such a file exists already, it will be overwritten
+    :type cleaned_datafile: string
+    """
+
+    # Load it
     df = pd.read_csv(raw_datafile)
     initial_shape = df.shape
 
     # Remove any leading/trailing blank from column names
-    mapper = {key: key.strip() for key in df.columns}
+    mapper = {key: key.lstrip() for key in df.columns}
     df.rename(mapper=mapper, axis='columns', inplace=True)
 
     # Get the list of columns containing strings (of type object)
-    str_column_names = [name for name in df.columns if pd.api.types.is_string_dtype(df[name])]
+    str_column_names = str_columns(df)
     # Remove any leading/trailing blank from strings in the dataframe, and replace unknwon values ('?') with a None
     for col_name in str_column_names:
-        df[col_name] = df[col_name].apply(lambda x: x.strip())
+        df[col_name] = df[col_name].apply(lambda x: x.lstrip())
         df[col_name].replace('?', None, inplace=True)
 
     # Replace the two categories in the `salary` column with 0 and 1
@@ -27,7 +53,17 @@ def main():
     assert df.shape == initial_shape
 
     # Save the cleaned dataset
+    if Path(cleaned_datafile).exists():
+        logging.info(f'Overwriting existing {cleaned_datafile}')
+    else:
+        logging.info(f'Writing {cleaned_datafile}')
     df.to_csv(cleaned_datafile, index=False, na_rep='null')
+
+
+def main():
+    raw_datafile = '../data/census.csv'
+    cleaned_datafile = '../data/census_cleaned.csv'
+    pre_process(raw_datafile, cleaned_datafile)
 
 
 if __name__ == '__main__':
--- src/train.py
+++ src/train.py
@@ -1,60 +1,118 @@
+import logging
 import pandas as pd
 import numpy as np
+from pathlib import Path
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import log_loss
+# from sklearn.metrics import log_loss
 from catboost import Pool, cv, CatBoostClassifier
-import logging
+from census_preprocess import pre_process, str_columns
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)-15s %(message)s")
 logger = logging.getLogger()
 
 seed = 42
+raw_datafile = '../data/census.csv'
 cleaned_datafile = '../data/census_cleaned.csv'
+saved_model = '../model/trained_model.bin'
 categorical_idx = [1, 3, 4, 5, 6, 7, 8, 9, 13]
-df = pd.read_csv(cleaned_datafile)
-# print(df.iloc[:, categorical_idx])
 
-str_column_names = [name for name in df.columns if pd.api.types.is_string_dtype(df[name])]
-# Remove any leading/trailing blank from strings in the dataframe, and replace unknwon values ('?') with a None
-for col_name in str_column_names:
-    df[col_name] = df[col_name].where(pd.notnull(df[col_name]), 'None')
-X = df.drop('salary', axis=1)
-y = df['salary']
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=.8, random_state=seed, stratify=y)
-logging.info(
-    f'Got {len(X_train)} samples in the training set ({sum(y_train)} positive) and {len(X_test)} in the test set ({sum(y_test)} positive).')
+def train_and_save(train_val_pool: Pool, filename: str, file_format: str = 'cbm') -> CatBoostClassifier:
+    """
+    Train and select a classification model based on cross-validation, save and return it. Before saving, the
+    selected model is re-trained on the whole training and validation dataset.
 
-train_val_pool = Pool(data=X_train, label=y_train, cat_features=categorical_idx)
-params = {}
-params['loss_function'] = 'Logloss'
-params['iterations'] = 50
-# params['custom_loss'] = 'AUC'
-params['random_seed'] = seed
-params['learning_rate'] = 0.5
+    :param train_val_pool: The CatBoost Pool holding the dataset for training and cross-validation.
+    :type train_val_pool: catboost.Pool
+    :param filename: The name of the file to be used to save the trained model.
+    :type filename: str
+    :param file_format: The type for the model save file, see CatBoost documentation for
+    [save_model](https://catboost.ai/en/docs/concepts/python-reference_catboost_save_model).
+    :type file_format: str
+    :return: The trained classification model, the same that went into the save file.
+    :rtype: catboost.CatBoostClassifier
+    """
+    params = {}
+    params['loss_function'] = 'Logloss'
+    params['iterations'] = 50
+    # params['custom_loss'] = 'AUC'
+    params['random_seed'] = seed
+    params['learning_rate'] = 0.5
 
-logging.info(f'Training with cross-validation')
-cv_data = cv(params=params,
-             pool=train_val_pool,
-             fold_count=5,
-             partition_random_seed=seed,
-             stratified=True,
-             verbose=True)
-best_iter = np.argmin(cv_data['test-Logloss-mean'])
-best_loss = cv_data.loc[best_iter, 'test-Logloss-mean']
-logging.info(f'Best iteration among all folds was #{best_iter} with test loss {best_loss}.')
-logging.info(f'Re-training the model for {best_iter + 1} iterations on the whole training/validation dataset')
+    logging.info(f'Training with cross-validation')
+    cv_data = cv(params=params,
+                 pool=train_val_pool,
+                 fold_count=5,
+                 partition_random_seed=seed,
+                 stratified=True,
+                 verbose=True)
+    best_iter = np.argmin(cv_data['test-Logloss-mean'])
+    best_loss = cv_data.loc[best_iter, 'test-Logloss-mean']
+    logging.info(f'Best iteration among all folds was #{best_iter} with test loss {best_loss}.')
+    logging.info(f'Re-training the model for {best_iter + 1} iterations on the whole training/validation dataset')
 
-params['iterations'] = best_iter + 1
-model = CatBoostClassifier(verbose=True,
-                           **params)
-model.fit(train_val_pool,
-          verbose=True)
+    params['iterations'] = best_iter + 1
+    best_model = CatBoostClassifier(verbose=True,
+                                    **params)
+    best_model.fit(train_val_pool,
+                   verbose=True)
 
-logging.info(f'Evaluating the model against the test set.')
-test_pool = Pool(data=X_test, label=y_test, cat_features=categorical_idx)
-y_prob = model.predict_proba(test_pool)
-# test_loss = model.eval_metrics(test_pool, params['loss_function'])
-test_loss = log_loss(y_test, y_prob)
-logging.info(f'Test loss is {test_loss}')
+    best_model.save_model(fname=filename,
+                          format=file_format,
+                          pool=train_val_pool if format in ('cpp', 'python', 'JSON') else None)
+    return best_model
 
+
+def test_model(file_name: str, test_pool: Pool, y_test: list, metrics: list[str]) -> dict[str,float]:
+    logging.info(
+        f'Evaluating model saved in {file_name} against a test set with {len(y_test)} samples ({sum(y_test)} positive).')
+    model = CatBoostClassifier()
+    model.load_model(file_name)
+    y_prob = model.predict_proba(test_pool)
+    # test_loss = log_loss(y_test, y_prob)
+    metrics = model.eval_metrics(data=test_pool,
+                                 metrics=metrics,
+                                 ntree_start=model.tree_count_ - 1)
+    # Change the values in the metrics dict, so they are scalars instead of lists of length 1
+    metrics = {key: value[0] for key, value in metrics.items()}
+    logging.info(f'Evaluation results:\n{metrics}')
+    return metrics
+
+
+def main():
+    if not Path(cleaned_datafile).exists():
+        logging.info(f'Pre-processed dataset {cleaned_datafile} not found. Going to make it now from raw dataset {raw_datafile}')
+        pre_process(raw_datafile, cleaned_datafile)
+
+    logging.info(f'Loading pre-processed dataset {cleaned_datafile}')
+    df = pd.read_csv(cleaned_datafile)
+
+    str_column_names = str_columns(df)
+    # Replace any null value in columns of type string with the string 'None', in order to be processed correctly
+    # by CatBoost
+
+    for col_name in str_column_names:
+        df[col_name] = df[col_name].where(pd.notnull(df[col_name]), 'None')
+
+    # Split the dataset into training and test set. The training set will be used for training and cross-validation.
+    X = df.drop('salary', axis=1)
+    y = df['salary']
+    X_train, X_test, y_train, y_test = train_test_split(X, y, train_size=.8, random_state=seed, stratify=y)
+    logging.info(
+        f'Got {len(X_train)} samples in the training set ({sum(y_train)} positive) and {len(X_test)} in the test set ({sum(y_test)} positive).')
+
+    # Make a Pool for CatBoost out of the training set
+    train_val_pool = Pool(data=X_train, label=y_train, cat_features=categorical_idx)
+
+    # Train and cross-validate the model, and save it in CatBoost binary format
+    model = train_and_save(train_val_pool=train_val_pool,
+                           filename=saved_model,
+                           file_format='cbm')
+
+    # Test the model
+    test_pool = Pool(data=X_test, label=y_test, cat_features=categorical_idx)
+    metrics = test_model(saved_model, test_pool, y_test, ['Logloss', 'AUC', 'F1', 'Recall', 'Precision', 'Accuracy'])
+
+
+if __name__ == '__main__':
+    main()
